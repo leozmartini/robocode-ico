f1046266724fac690f8aab7c707e5f7f21803dc6
Gabriel <trighy3@gmail.com>
2024-07-12 21:02:36 -0300

    feat: Alterada a cor + Atirar só a 15 pixels + Evitar parede

--- RobocopMamonas.java
+++ RobocopMamonas.java
@@ -1,7 +1,8 @@
 package robotsICO;
 import robocode.*;
+import java.awt.Color;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
-public class RobocopMamonas extends AdvancedRobot
+public class TanqueIncrivel extends AdvancedRobot
 {
 public void run() {
 // Initialization of the robot should be put here
@@ -10,16 +11,35 @@ public void run() {
 // and the next line:
 
 // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+setColors(Color.white,Color.white,Color.blue);
+
+
 
 // Robot main loop
 while(true) {
 // Replace the next 4 lines with any behavior you would like
 setAhead(100);
+setTurnRadarRight(360);
 setTurnRight(30);
 execute();
-turnRadarRight(360);
+avoidWalls();
 }
 }
+
+private void avoidWalls() {
+        // Check if close to any wall and adjust heading
+        if (getX() < 100) {
+            turnRight(90);
+        } else if (getX() > getBattleFieldWidth() - 100) {
+            turnLeft(90);
+        }
+
+        if (getY() < 100) {
+            turnRight(90);
+        } else if (getY() > getBattleFieldHeight() - 100) {
+            turnLeft(90);
+        }
+    }
 
 /**
 * onScannedRobot: What to do when you see another robot
@@ -30,11 +50,8 @@ double absoluteBearing = getHeading() + e.getBearing();
 double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 
 // If it's close enough, fire!
-if (Math.abs(bearingFromGun) <= 3) {
-setAhead(100);
-setTurnRight(30);
-execute();
-turnGunRight(bearingFromGun);
+if (Math.abs(bearingFromGun) <= 10) {
+fire(2);
 // We check gun heat here, because calling fire()
 // uses a turn, which could cause us to lose track
 // of the other robot.

58de428256cb510eca3737fc157e9cee4ad7c2db
demar-nbk <demartinileo@hotmail.com>
2024-07-22 16:39:11 -0300

    style: formatando codigo

--- RobocopMamonas.java
+++ RobocopMamonas.java
@@ -1,112 +1,117 @@
 package robotsICO;
-import robocode.*;
+
+import static robocode.util.Utils.normalRelativeAngleDegrees;
+
+import java.awt.*;
 import java.awt.Color;
+import robocode.*;
 import robocode.HitRobotEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 import robocode.WinEvent;
-import static robocode.util.Utils.normalRelativeAngleDegrees;
-
-import java.awt.*;
 
 public class RobocopMamonas extends AdvancedRobot {
-	int count = 0; // Keeps track of how long we've
-	// been searching for our target
-	double gunTurnAmt; // How much to turn our gun when searching
-	String trackName; // Name of the robot we're currently tracking
-	
-public void run() {
-// Initialization of the robot should be put here
 
-// After trying out your robot, try uncommenting the import at the top,
-// and the next line:
+  int count = 0; // Keeps track of how long we've
+  // been searching for our target
+  double gunTurnAmt; // How much to turn our gun when searching
+  String trackName; // Name of the robot we're currently tracking
 
-// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-setColors(Color.white,Color.white,Color.blue);
+  public void run() {
+    // Initialization of the robot should be put here
 
-// Prepare gun
-		trackName = null; // Initialize to not tracking anyone
-		setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
-		gunTurnAmt = 10; // Initialize gunTurn to 10
+    // After trying out your robot, try uncommenting the import at the top,
+    // and the next line:
 
-// Robot main loop
-while(true) {
-// Replace the next 4 lines with any behavior you would like
-// turn the Gun (looks for enemy)
-			turnGunRight(gunTurnAmt);
-			// Keep track of how long we've been looking
-			count++;
-			// If we've haven't seen our target for 2 turns, look left
-			if (count > 2) {
-				gunTurnAmt = -10;
-			}
-			// If we still haven't seen our target for 5 turns, look right
-			if (count > 5) {
-				gunTurnAmt = 10;
-			}
-			// If we *still* haven't seen our target after 10 turns, find another target
-			if (count > 11) {
-				trackName = null;
-			}
-		}
-	}
+    // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+    setColors(Color.white, Color.white, Color.blue);
 
-/**
-* onScannedRobot: What to do when you see another robot
-*/
-public void onScannedRobot(ScannedRobotEvent e) {
-	setAhead(100);
-	setTurnRight(30);
-	execute();
-// Replace the next line with any behavior you would like
-double absoluteBearing = getHeading() + e.getBearing();
-double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+    // Prepare gun
+    trackName = null; // Initialize to not tracking anyone
+    setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
+    gunTurnAmt = 10; // Initialize gunTurn to 10
 
-// If it's close enough, fire!
-if (Math.abs(bearingFromGun) <= 10) {
-fire(2);
-// We check gun heat here, because calling fire()
-// uses a turn, which could cause us to lose track
-// of the other robot.
-if (getGunHeat() == 0) {
-fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
-}
-} // otherwise just set the gun to turn.
-// Note:  This will have no effect until we call scan()
-else {
-turnGunRight(bearingFromGun);
-}
-// Generates another scan event if we see a robot.
-// We only need to call this if the gun (and therefore radar)
-// are not turning.  Otherwise, scan is called automatically.
-if (bearingFromGun == 0) {
-scan();
-}
-}
+    // Robot main loop
+    while (true) {
+      // Replace the next 4 lines with any behavior you would like
+      // turn the Gun (looks for enemy)
+      turnGunRight(gunTurnAmt);
+      // Keep track of how long we've been looking
+      count++;
+      // If we've haven't seen our target for 2 turns, look left
+      if (count > 2) {
+        gunTurnAmt = -10;
+      }
+      // If we still haven't seen our target for 5 turns, look right
+      if (count > 5) {
+        gunTurnAmt = 10;
+      }
+      // If we *still* haven't seen our target after 10 turns, find another target
+      if (count > 11) {
+        trackName = null;
+      }
+    }
+  }
 
-/**
-* onHitByBullet: What to do when you're hit by a bullet
-*/
-public void onHitByBullet(HitByBulletEvent e) {
-// Replace the next line with any behavior you would like
-setBack(50);
-setAhead(100);
-setTurnRight(30);
-execute();
-}
+  /**
+   * onScannedRobot: What to do when you see another robot
+   */
+  public void onScannedRobot(ScannedRobotEvent e) {
+    setAhead(100);
+    setTurnRight(30);
+    execute();
+    // Replace the next line with any behavior you would like
+    double absoluteBearing = getHeading() + e.getBearing();
+    double bearingFromGun = normalRelativeAngleDegrees(
+      absoluteBearing - getGunHeading()
+    );
 
-/**
-* onHitWall: What to do when you hit a wall
-*/
-public void onHitWall(HitWallEvent e) {
-// Replace the next line with any behavior you would like
-back(200);
-turnRight(90);
-}
-	public void onWin(WinEvent e) {
-		for (int i = 0; i < 50; i++) {
-			turnRight(30);
-			turnLeft(30);
-		}
-	}
+    // If it's close enough, fire!
+    if (Math.abs(bearingFromGun) <= 10) {
+      fire(2);
+      // We check gun heat here, because calling fire()
+      // uses a turn, which could cause us to lose track
+      // of the other robot.
+      if (getGunHeat() == 0) {
+        fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+      }
+    } // otherwise just set the gun to turn.
+    // Note:  This will have no effect until we call scan()
+    else {
+      turnGunRight(bearingFromGun);
+    }
+    // Generates another scan event if we see a robot.
+    // We only need to call this if the gun (and therefore radar)
+    // are not turning.  Otherwise, scan is called automatically.
+    if (bearingFromGun == 0) {
+      scan();
+    }
+  }
+
+  /**
+   * onHitByBullet: What to do when you're hit by a bullet
+   */
+  public void onHitByBullet(HitByBulletEvent e) {
+    // Replace the next line with any behavior you would like
+    setBack(50);
+    setAhead(100);
+    setTurnRight(30);
+    execute();
+  }
+
+  /**
+   * onHitWall: What to do when you hit a wall
+   */
+  public void onHitWall(HitWallEvent e) {
+    // Replace the next line with any behavior you would like
+    back(200);
+    turnRight(90);
+  }
+
+  public void onWin(WinEvent e) {
+    for (int i = 0; i < 50; i++) {
+      turnRight(30);
+      turnLeft(30);
+    }
+  }
 }

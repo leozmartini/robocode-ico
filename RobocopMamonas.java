194dbf17fb754bf4fd67910183bfd49400f35cfe
Otavio <otavmacedo04@gmail.com>
2024-07-22 21:06:17 -0300

    style: documentando as funcionalidades

--- RobocopMamonas.java
+++ RobocopMamonas.java
@@ -1,7 +1,6 @@
 package robotsICO;
 
 import static robocode.util.Utils.normalRelativeAngleDegrees;
-
 import java.awt.*;
 import robocode.*;
 
@@ -15,39 +14,45 @@ public class RobocopMamonas extends AdvancedRobot {
     setColors(Color.pink, Color.pink, Color.pink);
 
     trackName = null;
-    setAdjustGunForRobotTurn(true);
-    setAdjustRadarForGunTurn(true);
-    gunTurnAmt = 10;
+
+    setAdjustGunForRobotTurn(true); // O canhão não gira com o robô
+    setAdjustRadarForGunTurn(true); // O radar não gira com o canhão
+    gunTurnAmt = 10; // Quantidade inicial de rotação do canhão
 
     while (true) {
-      turnRadarRight(360);
+      turnRadarRight(360); // Busca inimigos girando o radar
     }
   }
 
   public void onScannedRobot(ScannedRobotEvent e) {
+    // Ajusta o radar para focar no inimigo
     double radarTurn = getHeading() + e.getBearing() - getRadarHeading();
     setTurnRadarRight(normalRelativeAngleDegrees(radarTurn));
 
+    // Calcula o ângulo absoluto para o inimigo
     double absoluteBearing = getHeading() + e.getBearing();
-    double bearingFromGun = normalRelativeAngleDegrees(
-      absoluteBearing - getGunHeading()
-    );
+    double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 
+    // Se o inimigo estiver a menos de 10 graus da linha de fogo, atira
     if (Math.abs(bearingFromGun) <= 10) {
       fire(2);
 
+      // Atira novamente se o canhão estiver frio
       if (getGunHeat() == 0) {
         fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
       }
     } else {
+      // Gira o canhão em direção ao inimigo
       setTurnGunRight(bearingFromGun);
     }
 
+    // Se o inimigo estiver perto, o robo sai de perto
     if (e.getDistance() < 100) {
       setBack(50);
       setTurnRight(90);
       setAhead(150);
     } else {
+      // Se o inimigo estiver longe, vai pra frente e gira
       setAhead(100);
       setTurnRight(30);
     }
@@ -68,6 +73,7 @@ public class RobocopMamonas extends AdvancedRobot {
   }
 
   public void onWin(WinEvent e) {
+    // Humilha o robo, dançando o reboleixon
     for (int i = 0; i < 50; i++) {
       ahead(20);
       turnRight(30);
